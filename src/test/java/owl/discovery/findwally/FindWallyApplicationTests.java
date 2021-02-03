d401ce90fa188faaf2ace11434304446aafe3206
Marco Righi <marco.righi@tasgroup.eu>
2021-02-03 18:58:02 +0100

    Initial commit

---
+++ src/test/java/owl/discovery/findwally/FindWallyApplicationTests.java
@@ -0,0 +1,13 @@
+package owl.discovery.findwally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindWallyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
